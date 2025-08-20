9fdba077454b97d1f21d493fb81941e88321b31b
Devashishbasu <devashishbasu587@gmail.com>
2025-08-20 15:58:19 +0530

    Removes unused imports
    removed unused imports

--- src/main/java/com/example/invoice_service/model/Dealer.java
+++ src/main/java/com/example/invoice_service/model/Dealer.java
@@ -2,9 +2,8 @@ package com.example.invoice_service.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 
 @Data
